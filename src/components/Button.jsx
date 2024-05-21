777e1433bd987ca49b495e834820f9d30d9325a4
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-21 18:58:59 -0300

    minor fix on the Button component responsiveness

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Button({ onClick }) {
     return (
-        <div
-            className="max-w-[340px] h-[45px] flex justify-center items-center bg-orange hover:bg-white text-dark-blue-3 font-bold uppercase rounded-3xl tracking-widest pt-1 cursor-pointer transition"
-            onClick={onClick}
-        >
-            Submit
+        <div className="w-full h-[45px] flex justify-center">
+            <div
+                className="w-[340px] h-[45px] flex justify-center items-center bg-orange hover:bg-white text-dark-blue-3 font-bold uppercase rounded-3xl tracking-widest pt-1 cursor-pointer transition"
+                onClick={onClick}
+            >
+                Submit
+            </div>
         </div>
     );
 }
