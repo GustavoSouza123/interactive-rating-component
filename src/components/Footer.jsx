602ce207f9a4f4745f4cc1b8ff60ecdf3d1a5b7e
Gustavo Souza <gustavoelia7@gmail.com>
2024-05-24 18:43:55 -0300

    fix: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="absolute z-50 bottom-0 w-full h-10 flex justify-center items-center text-light-grey">
-            <span className="mr-1">Challenge by <a className="text-orange" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.</span>
-            <span className="mr-1">Coded by <a className="text-orange" href="https://www.gustavo-souza.com" target="_blank">Gustavo Souza</a>.</span>
+        <footer className="absolute z-50 bottom-3 w-full h-10 text-light-grey text-center px-6">
+            Challenge by <a className="text-orange" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a className="text-orange" href="https://www.gustavo-souza.com" target="_blank">Gustavo Souza</a>.
         </footer>
     );
 }
